3883d31978b1838f7e41b31b6532855e71cce74a
Jing Lang <georgina_lang@yahoo.com>
2020-02-06 10:55:15 -0800

    samesize

--- routes/add.js
+++ routes/add.js
@@ -3,9 +3,9 @@ var data = require("../data.json");
 exports.addFriend = function(request, response) { 
 	var one = request.query.name;
 	var two = request.query.description;
-	var one = {'name': name, 'description': two, "imageURL": "http:://lorempixel.com/400/400/people"};
+	var one = {'name': one, 'description': two, "imageURL": "http:://lorempixel.com/400/400/people"};
 	console.log(one);
-	data.friends.push(one);
+	data.friends.push(newFriend);
 	response.render('index', data);
 
  }
